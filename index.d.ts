9dcf30721d39b9b01cde52cf8840e26e38799cfa
Dmitry Yv <df.creative@gmail.com>
2019-01-05 14:25:31 -0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,3 @@
 export = save;
 
-declare function save (data: ArrayBuffer, filename: string, done: function) : Promise
-
-declare function save (data: Buffer, filename: string, done: function) : Promise
-
-declare function save (data: TypedArray, filename: string, done: function) : Promise
-
-declare function save (data: Array, filename: string, done: function) : Promise
-
-declare function save (data: File, filename: string, done: function) : Promise
-
-declare function save (data: Blob, filename: string, done: function) : Promise
-
-declare function save (data: string, filename: string, done: function) : Promise
+declare function save (data: any, filename: any) : Promise;
